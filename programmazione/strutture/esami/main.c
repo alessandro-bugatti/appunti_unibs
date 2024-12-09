0fadae86cd07c26de5a04e8a0e58a27b822fbb45
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2024-12-09 12:08:06 +0100

    Preparato il codice dell'esempio degli esami

--- programmazione/strutture/esami/main.c
+++ programmazione/strutture/esami/main.c
@@ -7,6 +7,7 @@ typedef struct {
 }Data;
 
 #define MAX 200
+#define MAX_STUDENTI 10
 
 typedef struct {
     char nome[MAX];
@@ -22,19 +23,31 @@ typedef struct {
     int n_esami;
 }Studente;
 
+typedef struct {
+    Studente studenti[MAX_STUDENTI];
+    int n_studenti;
+} Registro;
+
+//Aggiungi esame
 void aggiungiEsame(Studente *s, Esame e) {
     s->esami[s->n_esami] = e;
     s->n_esami++;
 }
 
+//Ricerca studente
+
+//Aggiunta di un nuovo studente
+
+//Elenco degli studenti che hanno passato un certo esame
+
 void stampa(Studente s) {
     printf("%s %s matricola: %s\n",
         s.cognome, s.nome, s.matricola);
     if (s.n_esami == 0) {
-        printf("Nessn esame presente\n");
+        printf("Nessun esame presente\n");
     }
     else {
-        printf("NUmero esami sostenuti: %d\n",
+        printf("Numero esami sostenuti: %d\n",
             s.n_esami);
         for (int i = 0; i < s.n_esami; ++i) {
             printf("%d-%d-%d: %s voto: %d\n",
@@ -48,7 +61,49 @@ void stampa(Studente s) {
     }
 }
 
-int main(void) {
+void menu() {
+    printf("1 - Inserisci un nuovo studente\n");
+    printf("2 - Inserisci un nuovo esame\n");
+    printf("3 - Stampa registro\n");
+    printf("4 - Stampa dati studente\n");
+    printf("5 - Stampa passaggio esame\n");
+    printf("0 - Esci\n");
+    printf("Scegli l'opzione: \n");
 
+}
+
+int main(void) {
+    Registro registro = {
+        {
+            {
+                "123456",
+                "Rossi",
+                "Adele",
+                {},
+                0
+            },
+            {
+                "123457",
+                "Bianchi",
+                "Ottavio",
+                {},
+                0
+        }
+        },
+        2
+    };
+
+    int scelta;
+    menu();
+    scanf("%d", &scelta);
+    while (scelta != 0) {
+        if (scelta == 1) {
+
+        }
+
+        menu();
+        scanf("%d", &scelta);
+
+    }
     return 0;
 }
