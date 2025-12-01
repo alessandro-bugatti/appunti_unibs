50ece37bc26ac24ce198647e6b20a425be0e31be
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2025-12-01 17:49:47 +0100

    MOdifica la soluzione delle equazioni di secodo grado per utilizzare le strutture.

--- programmazione/funzioni/secondo_grado/main.c
+++ programmazione/funzioni/secondo_grado/main.c
@@ -3,6 +3,14 @@
 
 //da completare per gestire quando il delta è < 0
 
+//Versione nuova aggiunta utilizzando le strutture
+
+typedef struct
+{
+    double s1;
+    double s2;
+} Soluzioni;
+
 void sol_secondo_grado(double a, double b, double c,
                         double *s1, double *s2) {
     double delta = b * b - 4 * a * c;
@@ -10,10 +18,21 @@ void sol_secondo_grado(double a, double b, double c,
     *s2 = (- b - sqrt(delta)) / (2 * a);
 }
 
+Soluzioni sol_secondo_grado_nuova(double a, double b, double c) {
+    Soluzioni s;
+    double delta = b * b - 4 * a * c;
+    s.s1 = (- b + sqrt(delta)) / (2 * a);
+    s.s2 = (- b - sqrt(delta)) / (2 * a);
+    return s;
+}
+
 int main(void) {
     double a = 1000, b = 10, c = 2;
     double sol1, sol2;
     sol_secondo_grado(a, b, c, &sol1, &sol2);
     printf("s1: %lf, s2: %lf\n", sol1, sol2);
+    Soluzioni s = sol_secondo_grado_nuova(a, b, c);
+    printf("s1: %lf, s2: %lf\n", s.s1, s.s2);
+
     return 0;
 }
